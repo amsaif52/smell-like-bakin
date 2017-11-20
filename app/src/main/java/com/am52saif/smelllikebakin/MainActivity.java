d86998e28329420db0c8712c49e132b3c820374c
Ali Asgar Merchant <am52saif@gmail.com>
2017-11-19 19:53:04 -0500

    Initial commit

---
+++ app/src/main/java/com/am52saif/smelllikebakin/MainActivity.java
@@ -0,0 +1,13 @@
+package com.am52saif.smelllikebakin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
